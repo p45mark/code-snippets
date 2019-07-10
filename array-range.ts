f05cf4d8adba18ff88fef950878c0f6fae6fa131
Mark Barry <mark.barry@autogravity.com>
2019-07-10 15:41:52 -0700

    Rename func

--- array-range.ts
+++ array-range.ts
@@ -1,4 +1,4 @@
-const getRange = (start: number, length: number): number[] =>
+const arrayRange = (start: number, length: number): number[] =>
   Array(length)
     .fill(start)
     .map((a, b) => a + b);
